da47434e3d3ff00c937da6f53e0e351ea2076ee7
rmakiyama <rmakiyama.io@gmail.com>
2019-09-18 01:32:41 +0900

    add cast menu

--- app/src/main/java/com/rmakiyama/sample/exoplayer/cast/MainActivity.kt
+++ app/src/main/java/com/rmakiyama/sample/exoplayer/cast/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.rmakiyama.sample.exoplayer.cast
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.cast.framework.CastButtonFactory
+import com.google.android.gms.cast.framework.CastContext
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var castContext: CastContext
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        castContext = CastContext.getSharedInstance(this)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        CastButtonFactory.setUpMediaRouteButton(this, menu, R.id.menu_cast)
+        return true
     }
 }
