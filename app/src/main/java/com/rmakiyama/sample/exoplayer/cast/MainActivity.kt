f275cb23391c5ee4a7747649e67a3e370848a011
rmakiyama <rmakiyama.io@gmail.com>
2019-09-18 14:40:19 +0900

    add audio play

--- app/src/main/java/com/rmakiyama/sample/exoplayer/cast/MainActivity.kt
+++ app/src/main/java/com/rmakiyama/sample/exoplayer/cast/MainActivity.kt
@@ -1,19 +1,47 @@
 package com.rmakiyama.sample.exoplayer.cast
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.DefaultRenderersFactory
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import com.google.android.gms.cast.framework.CastButtonFactory
 import com.google.android.gms.cast.framework.CastContext
+import com.rmakiyama.sample.exoplayer.cast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
+    private val player: SimpleExoPlayer by lazy {
+        ExoPlayerFactory.newSimpleInstance(
+            this,
+            DefaultRenderersFactory(this),
+            DefaultTrackSelector(),
+            DefaultLoadControl()
+        )
+    }
     private lateinit var castContext: CastContext
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         castContext = CastContext.getSharedInstance(this)
-        setContentView(R.layout.activity_main)
+
+        binding.play.setOnClickListener { player.playWhenReady = true }
+        binding.pause.setOnClickListener { player.playWhenReady = false }
+
+        setupSource()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -21,5 +49,22 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu, menu)
         CastButtonFactory.setUpMediaRouteButton(this, menu, R.id.menu_cast)
         return true
+    }
+
+    private fun setupSource() {
+        val sourceFactory = DefaultDataSourceFactory(
+            this,
+            Util.getUserAgent(this, getString(R.string.app_name))
+        )
+        val list = resources.getStringArray(R.array.audio_urls).map { Uri.parse(it) }
+        val concatenatingMediaSource = ConcatenatingMediaSource()
+        list.onEach {
+            concatenatingMediaSource.addMediaSource(
+                ProgressiveMediaSource
+                    .Factory(sourceFactory)
+                    .createMediaSource(it)
+            )
+        }
+        player.prepare(concatenatingMediaSource)
     }
 }
